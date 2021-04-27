5e1068b77bf134b8d01ac64c7215bb2ec47aa0db
Kasper Johansen <37331486+kaspersmjohansen@users.noreply.github.com>
2021-04-27 10:30:56 +0200

    Create Cloud-Deployment-Setup.ps1

---
+++ Cloud-Deployment-Setup.ps1
@@ -0,0 +1,46 @@
+﻿$OSDCloudFolder = "D:\OSDCloud"
+
+If (!(Test-Path -Path $OSDCloudFolder))
+{
+New-Item -Path $OSDCloudFolder -ItemType Directory -Verbose
+}
+
+Import-Module PowerShellGet -Force
+Install-Module -Name OSD -force
+New-OSDCloud.workspace -WorkspacePath $OSDCloudFolder
+
+# Autopilot creds
+$Autopilotcreds = Get-Credential
+Install-module Microsoft.Graph -Force
+Connect-MSGraph -Credential $Autopilotcreds
+$Autopilotdisplayname = (Get-AutopilotProfile).displayname
+
+# Autopilot configuration
+Get-AutopilotProfile | Where-Object DisplayName -eq $Autopilotdisplayname | ConvertTo-AutopilotConfigurationJSON | Out-File -FilePath "$OSDCloudFolder\AutoPilot\Profiles\AutoPilotConfigurationFile.json" -Encoding ASCII
+
+Edit-OSDCloud.winpe -WorkspacePath $OSDCloudFolder
+
+# Create wallpaper folder and download wallpaper
+If (!(Test-Path -Path "$OSDCloudFolder\Wallpaper"))
+{
+New-Item -Path "$OSDCloudFolder\Wallpaper" -ItemType Directory -Verbose
+}
+Invoke-WebRequest -Uri https://github.com/kaspersmjohansen/Cloud-Deployment/raw/main/wallpaper.jpg -OutFile "$OSDCloudFolder\Wallpaper\wallpaper.jpg"
+
+# Cloud drivers
+# Edit-OSDCloud.winpe -CloudDriver Dell,Nutanix,VMware
+
+# Your own drivers
+# Edit-OSDCloud.winpe -DriverPath $OSDCloudFolder
+
+
+# New-OSDCloud.template -WinRE -Language da-dk -SetAllIntl da-dk -SetInputLocale da-dk -verbose
+New-OSDCloud.template -WinRE -SetInputLocale da-dk -verbose
+New-OSDCloud.workspace -workspacepath $OSDCloudFolder -Verbose
+
+Edit-OSDCloud.winpe -workspacepath $OSDCloudFolder -WebPSScript https://raw.githubusercontent.com/kaspersmjohansen/Cloud-Deployment/main/Cloud-Deployment.ps1 -wallpaper "$OSDCloudFolder\Wallpaper\wallpaper.jpg" -Verbose
+
+
+
+# Create boot ISO
+New-OSDCloud.iso -workspacepath $OSDCloudFolder
