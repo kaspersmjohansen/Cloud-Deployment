02193137b3cc97e707fea14b7d9837b12e7f072a
Kasper Johansen <37331486+kaspersmjohansen@users.noreply.github.com>
2021-05-08 19:30:15 +0200

    Update Cloud-Deployment.ps1

--- Cloud-Deployment.ps1
+++ Cloud-Deployment.ps1
@@ -4,7 +4,7 @@ Start-Sleep -Seconds 5
 #Change Display Resolution for Virtual Machine
 if ((Get-MyComputerModel) -match 'Virtual') {
     Write-Host  -ForegroundColor Green "Setting Display Resolution to 1600x"
-    Set-DisRes 1600
+    Set-DisRes 1440
 }
 
 #Make sure I have the latest OSD Content
